7b519c79e1c32a77380aab2db0967b15ce63e04e
peardox <simon@peardox.com>
2024-09-07 03:06:10 +0100

    Make compatible with VSCODE and add default cert.c

--- src/u2f.c
+++ src/u2f.c
@@ -667,8 +667,7 @@ uint16_t tud_hid_get_report_cb(uint8_t instance, uint8_t report_id, hid_report_t
   return 0;
 }
 
-void tud_hid_report_complete_cb(uint8_t instance, const uint8_t *report,
-        uint8_t len)
+void tud_hid_report_complete_cb(uint8_t instance, uint8_t const* report, uint16_t len)
 {
     (void) instance;
     (void) len;
