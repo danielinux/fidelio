2219959b0d1f8cce71d0e4105aa93f3a1f1adc8a
peardox <simon@peardox.com>
2024-09-10 12:24:57 +0100

    Remove \ chars from mkcert.sh to make more Win friendly

--- mkcert.sh
+++ mkcert.sh
@@ -1,9 +1,6 @@
-openssl req -newkey ec -pkeyopt ec_paramgen_curve:"P-256" -keyout cert-master-key.pem \
-    -out cert-att.csr -sha256 -nodes \
-    -config attestation-cert.conf
+openssl req -newkey ec -pkeyopt ec_paramgen_curve:"P-256" -keyout cert-master-key.pem -out cert-att.csr -sha256 -nodes -config attestation-cert.conf
 
-openssl x509 -req -in cert-att.csr -signkey cert-master-key.pem -out cert-att.pem -sha256 -days 3650 \
-    -extfile attestation-cert.conf -extensions v3_req
+openssl x509 -req -in cert-att.csr -signkey cert-master-key.pem -out cert-att.pem -sha256 -days 3650 -extfile attestation-cert.conf -extensions v3_req
 
 openssl x509 -in cert-att.pem -out cert-att.der -outform der
 openssl ec -in cert-master-key.pem -out cert-master-key.der -outform der -conv_form uncompressed
